c771cf4a17df65f2ceb057bbb7eb8a42955f8a41
Aleksa <lukicaleksa04@gmail.com>
2020-04-02 13:32:35 +0200

    Dodati komentari na reg servisu

--- src/app/service/registration.service.ts
+++ src/app/service/registration.service.ts
@@ -12,9 +12,12 @@ export class RegistrationService {
     return this.http.post('api/registration/client', user, { responseType: 'text' })
   }
 
+  // Servis za registraciju kopanije
   registerCompany(user){
     return this.http.post('api/registration/company', user, { responseType: 'text' })
   }
+
+  // Servis za registraciju klijenta
   getUserType() {
     return this.http.get('api/registration/getUserType', { responseType: 'json' })
   }
