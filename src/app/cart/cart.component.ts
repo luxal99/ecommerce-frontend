d43cb831edfde437d198e4fed52c2a3c9c40d6be
Aleksa <lukicaleksa04@gmail.com>
2020-04-04 14:54:19 +0200

    Dodate funckije count i decrease

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-cart',
@@ -9,9 +10,10 @@ import { ProductService } from '../service/product.service';
 export class CartComponent implements OnInit {
 
   cartList:any=[];
+  amountCounter = 0;
   total = 0;
 
-  constructor(public productService:ProductService) { }
+  constructor(public productService:ProductService,private _snackBar: MatSnackBar ) { }
 
   ngOnInit() {
     this.getCart();
@@ -26,6 +28,30 @@ export class CartComponent implements OnInit {
 
   getTotal(){
     this.total =this.productService.sum();
+  }
+
+  count(product) {
+    if (this.amountCounter === product.amount) {
+      this.openSnackBar(new Error("Max amount").message, "DONE")
+    } else {
+
+      this.amountCounter++;
+    }
+  }
+
+  decrease() {
+    if (this.amountCounter === 0) {
+      this.openSnackBar(new Error("Amount can not be less than zero").message, "DONE")
+    } else {
+
+      this.amountCounter--;
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
 
